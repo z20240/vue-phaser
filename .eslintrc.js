57069bb0975258acaa10af88930dc504a6556cd9
z20240 <z20240z@gmail.com>
2019-07-11 03:37:13 +0800

    e

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
         "ecmaVersion": 2018,
         "sourceType": "module",
         "extends": "standard",
-	    "parser": "babel-eslint"
+        "parser": "babel-eslint"
     },
     "plugins": [
         "vue"
