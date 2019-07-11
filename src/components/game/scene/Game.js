ab82e789e66fd5d57299eef2b6c636f1f349bf63
Les Lee <les.lee@slotfactory.com>
2019-07-11 13:23:19 +0800

    webpack config to make base64 image can be read.

--- src/components/game/scene/Game.js
+++ src/components/game/scene/Game.js
@@ -19,17 +19,15 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-        console.log(star);
-        console.log(sky);
-        console.log(dude);
+
         console.log("bomb1", bomb1);
         /** 在 preload 中預載圖片 */
         this.load.image('sky', sky);
         // this.load.image('dude', dude);
         // this.textures.addBase64('dude', dude);
-        // this.load.spritesheet('dude', './assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-        this.textures.addBase64('star', star);
-        this.textures.addBase64('bomb', bomb);
+        this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
+        this.load.image('star', star);
+        this.load.image('bomb', bomb);
         this.load.image('platform', platform);
     }
 
@@ -38,8 +36,8 @@ export default class Game extends Phaser.Scene {
         // 可以使用 setOrigin 設置原點來改變原點的起始位置。
         this.add.image(0, 0, 'sky').setOrigin(0, 0);
         this.add.image(100, 300, 'platform');
-        this.add.image(10, 10, 'dude');
-        this.add.image(200, 300, 'star');
+        this.add.image(50, 50, 'dude');
+        this.add.image(500, 500, 'star');
         this.add.image(200, 300, 'bomb');
     }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,26 @@
-const URL_Loader = require('url-loader');
-
 // vue.config.js
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            URL_Loader,
+    chainWebpack: config => {
+        /* disable insertion of assets as data urls b/c Phaser doesn't support it */
+        const rules = [
+            { name: 'images', dir: 'img' },
+            { name: 'media', dir: 'media' }
         ]
+        rules.forEach(rule => {
+            const ruleConf = config.module.rule(rule.name)
+
+            ruleConf.uses.clear()
+
+            ruleConf
+                .use('file-loader')
+                .loader('file-loader')
+                .options({
+                    name: `${rule.dir}/[name].[hash:8].[ext]`
+                })
+        })
+    },
+    devServer: {
+        open: true,
+        hot: false
     }
 }
